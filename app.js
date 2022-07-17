846b5c879b962cd8fbdfeaa1fd895b023d6e9a45
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-07-17 08:37:21 -0500

    updated code to reflect code snapshot

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
-
-// const printProfileData = (profileDataArr) => {
-//     console.log(profileDataArr);
-// };
+const profileDataArgs = process.argv.slice(2);
 
 const printProfileData = profileDataArr => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
 
-    // This...
+  console.log('================');
 
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-
-    console.log('===============');
-
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => {
-        console.log(proifleItem)
-    });
-
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
 };
-
-
-
 
 printProfileData(profileDataArgs);
