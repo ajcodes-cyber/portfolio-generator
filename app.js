8d5faf74799ed0714ac62b72493da566ce238ea9
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-07-17 19:11:27 -0500

    adding end of 9.2.5

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 const [ name, github] = profileDataArgs;
+const fs = require('fs');
 // const name = profileDataArgs[0];
 // const github = profileDataArgs[1];
 
@@ -21,8 +22,15 @@ const generatePage = (userName, githubName) => {
   </html>
   `;
 };
-console.log(name, github);
-console.log(generatePage(name, github));
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+// console.log(name, github);
+// console.log(generatePage(name, github));
 
 // const printProfileData = profileDataArr => {
 //   // This...
