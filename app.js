94a927825eaac317c2b9f1b8642f82dc080f4381
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-07-18 20:00:57 -0500

    adding because terminal says I need to commit before switching branches

--- app.js
+++ app.js
@@ -1,47 +1,12 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [ name, github] = profileDataArgs;
 const fs = require('fs');
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
+const generatePage = require('./src/page-template');
 
-const generatePage = (userName, githubName) => {
-  return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
+const profileDataArgs = process.argv.slice(2);
 
-  <body>
-    <h1>${name}</h1>
-    <h2><a href="https://github.com/${github}">Github</a></h2>
-  </body>
-  </html>
-  `;
-};
+const [name, github] = profileDataArgs;
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
-
-// console.log(name, github);
-// console.log(generatePage(name, github));
-
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
-
-//   console.log('================');
-
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
