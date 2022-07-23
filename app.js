8fe7e11fe6af21a5e3da5b5a722b9658c02223cb
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-07-23 12:09:22 -0500

    middle of 9.3.5

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-// Create new feature branch
-// Install the Inquirer package
-// Create the .gitignore file
-// Validate answers
-// Save your progress with git
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
+// const pageHTML = generatePage(name, github);
 
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-const profileDataArgs = process.argv.slice(2);
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-const [name, github] = profileDataArgs;
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
